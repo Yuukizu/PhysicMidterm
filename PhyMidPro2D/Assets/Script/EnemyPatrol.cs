60dd96161e4e5eef2a88aed69069d6caa7f5505b
Yuukizu <metineenoni@gmail.com>
2024-03-09 15:01:26 +0700

    Yay enemy patrol

---
+++ PhyMidPro2D/Assets/Script/EnemyPatrol.cs
@@ -0,0 +1,58 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] private GameObject PointA;
+    [SerializeField] private GameObject PointB;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = PointB.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+
+        if (currentPoint == PointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
+        {
+            flip();
+            currentPoint = PointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
+        {
+            flip();
+            currentPoint = PointB.transform;
+        }
+    }
+    private void flip()
+    {
+        Vector2 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
+        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
+
+    }
+}
+
