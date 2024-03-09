f2787b8114884f2cae868b6573323a1bfeb4f668
Yuukizu <metineenoni@gmail.com>
2024-03-09 15:39:11 +0700

    MuiCANMOVE

--- PhyMidPro2D/Assets/Script/EnemyPatrol.cs
+++ PhyMidPro2D/Assets/Script/EnemyPatrol.cs
@@ -1,6 +1,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
@@ -30,29 +31,28 @@ public class EnemyPatrol : MonoBehaviour
         {
             rb.velocity = new Vector2(-speed, 0);
         }
-        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 5f && currentPoint == PointB.transform)
         {
             flip();
             currentPoint = PointA.transform;
         }
-        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
+        if (Vector2.Distance(transform.position, currentPoint.position) < 5f && currentPoint == PointA.transform)
         {
             flip();
             currentPoint = PointB.transform;
         }
+        
     }
+
     private void flip()
     {
         Vector2 localScale = transform.localScale;
         localScale.x *= -1;
         transform.localScale = localScale;
     }
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
-        Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
-        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
 
-    }
+
+
 }
 
